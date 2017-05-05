eac218d8f1008e93506c44bef54af89a9d4f9bd2
Minchul Lee <bab2min@gmail.com>
2017-05-05 23:26:57 +0900

    upload 0.3
    Former-commit-id: 7813dfa98134e4ab02da3710ddcddf451b75a496

--- KiwiLibrary/KMemory.h
+++ KiwiLibrary/KMemory.h
@@ -1,6 +1,6 @@
 #pragma once
 
-template<size_t objectSize, size_t poolSize>
+template<size_t objectSize, size_t poolSize, size_t type = 0>
 class KPool
 {
 public:
@@ -9,13 +9,16 @@ public:
 
 	static KPool& getInstance()
 	{
-		static thread_local KPool inst;
+		thread_local KPool inst;
 		return inst;
 	}
 
 	void* allocate()
 	{
-		if (!freeList) throw bad_alloc();
+		if (!freeList)
+		{
+			throw bad_alloc();
+		}
 		void* p = freeList;
 		freeList = (void**)(*freeList);
 		return p;
@@ -43,7 +46,7 @@ private:
 	KPool(const KPool&) {}
 	~KPool()
 	{
-		delete[poolSize*objectSize] poolBuf;
+		delete[] poolBuf;
 	}
 };
 
@@ -55,7 +58,7 @@ public:
 	map<size_t, size_t> maxAlloc;
 	static KSingleLogger& getInstance()
 	{
-		static thread_local KSingleLogger inst;
+		thread_local KSingleLogger inst;
 		return inst;
 	}
 private:
@@ -140,8 +143,8 @@ class spool_allocator : public allocator<T>
 {
 public:
 	typedef size_t size_type;
-	typedef value_type* pointer;
-	typedef const value_type* const_pointer;
+	typedef T* pointer;
+	typedef const T* const_pointer;
 
 	template<typename _Tp1>
 	struct rebind
@@ -151,21 +154,21 @@ public:
 
 	pointer allocate(size_type n, const void *hint = 0)
 	{
-		if (n <= 32) return (pointer)KPool<32, 4096>::getInstance().allocate();
-		if (n <= 48) return (pointer)KPool<48, 4096>::getInstance().allocate();
-		return allocator<value_type>::allocate(n, hint);
+		if (n <= 32) return (pointer)KPool<32, 4000>::getInstance().allocate();
+		if (n <= 48) return (pointer)KPool<48, 4000>::getInstance().allocate();
+		return allocator<T>::allocate(n, hint);
 	}
 
 	void deallocate(pointer p, size_type n)
 	{
-		if (n <= 32) return KPool<32, 4096>::getInstance().deallocate(p);
-		if (n <= 48) return KPool<48, 4096>::getInstance().deallocate(p);
-		return allocator<value_type>::deallocate(p, n);
+		if (n <= 32) return KPool<32, 4000>::getInstance().deallocate(p);
+		if (n <= 48) return KPool<48, 4000>::getInstance().deallocate(p);
+		return allocator<T>::deallocate(p, n);
 	}
 
-	spool_allocator() throw() : allocator<value_type>() { /*fprintf(stderr, "Hello allocator!\n");*/ }
-	spool_allocator(const spool_allocator &a) throw() : allocator<value_type>(a) { }
+	spool_allocator() throw() : allocator<T>() { /*fprintf(stderr, "Hello allocator!\n");*/ }
+	spool_allocator(const spool_allocator &a) throw() : allocator<T>(a) { }
 	template <class U>
-	spool_allocator(const spool_allocator<U> &a) throw() : allocator<value_type>(a) { }
+	spool_allocator(const spool_allocator<U> &a) throw() : allocator<T>(a) { }
 	~spool_allocator() throw() { }
 };
