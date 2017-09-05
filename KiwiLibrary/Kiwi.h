3ed7be2afa83e90177f2ecf9cead6f849367f294
Minchul Lee <bab2min@gmail.com>
2017-09-06 02:53:21 +0900

    gm bug...
    Former-commit-id: 155404eaae0eeb12e6d4ebfd93f9f2c1745c28be

--- KiwiLibrary/Kiwi.h
+++ KiwiLibrary/Kiwi.h
@@ -24,6 +24,7 @@ protected:
 	static vector<const KChunk*> divideChunk(const k_vchunk& ch);
 	vector<k_vpcf> calcProbabilities(const KChunk* pre, const KChunk* begin, const KChunk* end, const char* ostr, size_t len) const;
 	vector<KInterResult> analyzeJM(const k_string& jm, size_t topN, KPOSTag prefix, KPOSTag suffix) const;
+	vector<KInterResult> analyzeJM2(const k_string& jm, size_t topN, KPOSTag prefix, KPOSTag suffix) const;
 	bool addCache(const string& jm, const vector<KInterResult>& value) const;
 	vector<KInterResult>* findCache(const string& jm) const;
 public:
@@ -40,7 +41,7 @@ public:
 	KResult analyze(const string& str) const;
 	vector<KResult> analyze(const k_wstring& str, size_t topN) const;
 	vector<KResult> analyze(const string& str, size_t topN) const;
-
+	vector<KResult> analyzeOld(const k_wstring& str, size_t topN) const;
 	vector<KResult> analyzeGM(const k_wstring& str, size_t topN) const;
 	void clearCache();
 };
