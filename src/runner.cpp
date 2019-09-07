9f3b899804172aca3ddfb430c01c7912db0347a9
Minchul Lee <bab2min@gmail.com>
2019-09-07 23:02:59 +0900

    add cmake for compiling at linux (#5)
    Former-commit-id: ac23dc807d5f6fa1eb6f96dcde686a043b8ccd19

--- src/runner.cpp
+++ src/runner.cpp
@@ -38,7 +38,7 @@ unordered_map<string, vector<string>> parseArg(int argc, const char** argv)
 void help()
 {
 	printf("Kiwi : Korean Intelligent Word Identifier\n"
-		"version: 0.5\n"
+		"version: 0.%d.%d\n"
 		"\n"
 		"= Usage =\n"
 		"kiwi [-m model] [-u user] [-o output] [-c cache] [-n number] input...\n"
@@ -46,7 +46,7 @@ void help()
 		"\tuser: (Optional) path of user dictionary file\n"
 		"\toutput: (Optional) path of result file\n"
 		"\tnumber: (Optional) the number of analyzed result, default = 1\n"
-		"\tinput: path of input file to be analyzed\n");
+		"\tinput: path of input file to be analyzed\n", Kiwi::getVersion() / 10, Kiwi::getVersion() % 10);
 }
 
 int main(int argc, const char** argv)
