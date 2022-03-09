d0c4cca3e4ec2c5cabaebc1a325bb9e38dd23d0d
Minchul Lee <bab2min@gmail.com>
2022-03-09 12:00:08 +0900

    implemented data augmentation

--- ModelGenerator/convertModuToKiwi.py
+++ ModelGenerator/convertModuToKiwi.py
@@ -27,6 +27,41 @@ def convert(input_file, output_file):
                 print(*s, sep='\t', end='\n', file=output_file)
             print(file=output_file, flush=True)
 
+def augment_ef(input_file, output_file, concat_sents=3):
+    obj = json.load(input_file)
+    for doc in obj['document']:
+        d = []
+        n = 0
+        for sent in doc['sentence']:
+            morphs = sent['morpheme']
+            try:
+                if morphs[-2]['label'] == 'EF' and morphs[-1]['label'] == 'SF':
+                    del morphs[-1]
+                elif morphs[-3]['label'] == 'EF' and morphs[-2]['label'] == 'JX' and morphs[-1]['label'] == 'SF':
+                    morphs[-3]['form'] += morphs[-2]['form']
+                    del morphs[-2]
+                    del morphs[-1]
+                else:
+                    continue
+            except:
+                continue
+            for word_id, word in enumerate(sent['word'] or [], start=1):
+                s = []
+                for morph in morphs:
+                    if morph['word_id'] != word_id: continue
+                    s.extend(normalize_morpheme(morph['form'], morph['label']))
+                d.append((word['form'], s))
+            n += 1
+            if concat_sents and n % concat_sents == 0:
+                for w, s in d:
+                    print(w, *s, sep='\t', end='\n', file=output_file)
+                print(file=output_file, flush=True)
+                d.clear()
+        if d:
+            for w, s in d:
+                print(w, *s, sep='\t', end='\n', file=output_file)
+            print(file=output_file, flush=True)
+
 def main(args):
     if args.output:
         output = open(args.output, 'w', encoding='utf-8')
@@ -34,7 +69,10 @@ def main(args):
         output = sys.stdout
     
     for input in args.input:
-        convert(open(input, encoding='utf-8'), output)
+        if args.augment_ef:
+            augment_ef(open(input, encoding='utf-8'), output)
+        else:
+            convert(open(input, encoding='utf-8'), output)
     
     if args.output:
         output.close()
@@ -44,5 +82,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', nargs='+')
     parser.add_argument('--output')
+    parser.add_argument('--augment_ef', default=False, action='store_true')
 
     main(parser.parse_args())
