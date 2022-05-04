a580f3f88c623122b33556e035480e0ce30bf95a
bab2min <bab2min@gmail.com>
2022-05-04 09:59:32 +0900

    added additional args to evaluator

--- tools/evaluator_main.cpp
+++ tools/evaluator_main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 
 #include <kiwi/Utils.h>
@@ -11,7 +11,7 @@
 using namespace std;
 using namespace kiwi;
 
-int doEvaluate(const string& modelPath, const string& output, const vector<string>& input, bool useSBG)
+int doEvaluate(const string& modelPath, const string& output, const vector<string>& input, bool normCoda, bool useSBG)
 {
 	try
 	{
@@ -29,7 +29,7 @@ int doEvaluate(const string& modelPath, const string& output, const vector<strin
 			cout << "Test file: " << tf << endl;
 			try
 			{
-				Evaluator test{ tf, &kw };
+				Evaluator test{ tf, &kw, normCoda ? Match::allWithNormalizing : Match::all};
 				tutils::Timer total;
 				test.run();
 				double tm = total.getElapsed();
@@ -88,12 +88,14 @@ int main(int argc, const char* argv[])
 
 	ValueArg<string> model{ "m", "model", "Kiwi model path", false, "ModelGenerator", "string" };
 	ValueArg<string> output{ "o", "output", "output dir for evaluation errors", false, "", "string" };
+	SwitchArg withoutNormCoda{ "", "wcoda", "without normalizing coda", false };
 	SwitchArg useSBG{ "", "sbg", "use SkipBigram", false };
 	UnlabeledMultiArg<string> files{ "files", "evaluation set files", true, "string" };
 
 	cmd.add(model);
 	cmd.add(output);
 	cmd.add(files);
+	cmd.add(withoutNormCoda);
 	cmd.add(useSBG);
 
 	try
@@ -105,6 +107,6 @@ int main(int argc, const char* argv[])
 		cerr << "error: " << e.error() << " for arg " << e.argId() << endl;
 		return -1;
 	}
-	return doEvaluate(model, output, files.getValue(), useSBG);
+	return doEvaluate(model, output, files.getValue(), !withoutNormCoda, useSBG);
 }
 
