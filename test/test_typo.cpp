f081033d3e97e75cb3f5c1924f888caad61abe30
bab2min <bab2min@gmail.com>
2022-06-01 18:18:25 +0900

    added test cases for Typo Features

---
+++ test/test_typo.cpp
@@ -0,0 +1,34 @@
+﻿#include "gtest/gtest.h"
+#include "common.h"
+#include <kiwi/Kiwi.h>
+#include <kiwi/TypoTransformer.h>
+
+using namespace kiwi;
+
+TEST(KiwiTypo, Generate)
+{
+	TypoTransformer tt;
+	tt.addTypo(u"ㅐㅔ", u"ㅐㅔ");
+
+	UnorderedMap<std::u16string, float> typos;
+	
+	for (auto e : tt.generate(u"개가납네"))
+	{
+		typos.emplace(e);
+	}
+
+	EXPECT_EQ(typos.size(), 4);
+	EXPECT_EQ(typos.find(u"개가납네")->second, 0);
+	EXPECT_EQ(typos.find(u"게가납네")->second, 1);
+	EXPECT_EQ(typos.find(u"개가납내")->second, 1);
+	EXPECT_EQ(typos.find(u"게가납내")->second, 2);
+}
+
+TEST(KiwiTypo, Builder)
+{
+	TypoTransformer tt;
+	tt.addTypo(u"ㅐㅔ", u"ㅐㅔ");
+	Kiwi kiwi = KiwiBuilder{ MODEL_PATH, 0, BuildOption::default_, }.build(tt);
+
+	kiwi.analyze(u"문화제 보호", Match::allWithNormalizing);
+}
