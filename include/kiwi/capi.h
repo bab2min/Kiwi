b4610fb24e7606ff59818e14abb7b5f3c505d6c0
bab2min <bab2min@gmail.com>
2022-08-31 01:40:09 +0900

    updated `kiwi_token_info_t` in C API

--- include/kiwi/capi.h
+++ include/kiwi/capi.h
@@ -45,6 +45,8 @@ typedef struct {
 	float score; /**< 해당 형태소의 언어모델 점수 */
 	float typo_cost; /**< 오타가 교정된 경우 오타 비용. 그렇지 않은 경우 0 */
 	uint_fast32_t typo_form_id; /**< 교정 전 오타의 형태에 대한 정보 (typoCost가 0인 경우 의미 없음) */
+	uint_fast32_t paired_token; /**< SSO, SSC 태그에 속하는 형태소의 경우 쌍을 이루는 반대쪽 형태소의 위치(-1인 경우 해당하는 형태소가 없는 것을 뜻함) */
+	uint_fast32_t sub_sent_position; /**< 인용부호나 괄호로 둘러싸인 하위 문장의 번호. 1부터 시작. 0인 경우 하위 문장이 아님을 뜻함 */
 } kiwi_token_info_t;
 
 /*
