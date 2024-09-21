c0d940ffdc6681216bb78de8f6817be09fffb8fe
bab2min <bab2min@gmail.com>
2024-09-22 03:32:08 +0900

    Implement `NgramExtractor`

--- include/kiwi/SubstringExtractor.h
+++ include/kiwi/SubstringExtractor.h
@@ -62,4 +62,49 @@ namespace kiwi
 		size_t cluster(size_t i) const;
 		float score(size_t i) const;
 	};
+
+	class Kiwi;
+
+	class NgramExtractor
+	{
+		const Kiwi* kiwi = nullptr;
+		bool gatherLmScore = true;
+		UnorderedMap<std::u16string, size_t> morph2id;
+		Vector<std::u16string> id2morph;
+		Vector<uint16_t> buf;
+		Vector<int16_t> scores;
+		Vector<size_t> docBoundaries;
+		Vector<uint32_t> positions;
+		Vector<std::u16string> rawDocs;
+
+		size_t addTokens(const std::vector<TokenInfo>& tokens);
+
+	public:
+		struct Candidate
+		{
+			std::u16string text;
+			std::vector<std::u16string> tokens;
+			std::vector<float> tokenScores;
+			size_t cnt = 0;
+			size_t df = 0;
+			float score = 0;
+			float npmi = 0;
+			float leftBranch = 0;
+			float rightBranch = 0;
+			float lmScore = 0;
+		};
+
+		NgramExtractor();
+		NgramExtractor(const Kiwi& kiwi, bool gatherLmScore = true);
+		NgramExtractor(const NgramExtractor&);
+		NgramExtractor(NgramExtractor&&) noexcept;
+		NgramExtractor& operator=(const NgramExtractor&);
+		NgramExtractor& operator=(NgramExtractor&&) noexcept;
+		~NgramExtractor();
+
+		size_t addText(const std::u16string& text);
+		size_t addTexts(const U16Reader& reader);
+
+		std::vector<Candidate> extract(size_t maxCandidates = 1000, size_t minCnt = 10, size_t maxLength = 5, float minScore = 1e-3, size_t numWorkers = 1) const;
+	};
 }
