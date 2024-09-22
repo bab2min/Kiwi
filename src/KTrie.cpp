61bf5de05e2194e6a3755158f5b086e13c2fced5
bab2min <bab2min@gmail.com>
2024-09-22 21:44:17 +0900

    Optimize `splitByTrie`

--- src/KTrie.cpp
+++ src/KTrie.cpp
@@ -972,16 +972,6 @@ size_t kiwi::splitByTrie(
 			if (curNode->fail())
 			{
 				curNode = curNode->fail();
-				for (auto submatcher = curNode; submatcher; submatcher = submatcher->fail())
-				{
-					const Form* cand = submatcher->val(trie);
-					if (!cand) break;
-					else if (!trie.hasSubmatch(cand))
-					{
-						zCodaFollowable = zCodaFollowable || getZCodaAppendable<typoTolerant>(cand, formBase);
-						if (!insertCandidates(candidates, cand, formBase, typoPtrs, str, nonSpaces)) break;
-					}
-				}
 				nextNode = curNode->template nextOpt<arch>(trie, c);
 			}
 			else
