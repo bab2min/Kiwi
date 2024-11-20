89f4015554dc221fd77dd8b23b510f9c662d3510
bab2min <bab2min@gmail.com>
2024-11-20 23:51:38 +0900

    bump version to 0.20.2

--- bindings/java/kr/pe/bab2min/Kiwi.java
+++ bindings/java/kr/pe/bab2min/Kiwi.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Kiwi implements AutoCloseable  {
 	private long _inst;
-	final private static String _version = "0.20.1";
+	final private static String _version = "0.20.2";
 
 	public static class Match {
 		final static public int none = 0,
