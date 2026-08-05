9a7498f8b42d163dfc87eba2d263b88acfdc6aa5
Hakhyun Kim <hakhyun.2023@gmail.com>
2026-08-05 11:42:10 +0900

    Add test cases for Kiwi::glue
    The expected values are taken from kiwipiepy 0.23.2, including the example
    shown in the docstring of `Kiwi.glue`. They also cover the cases that are
    easy to get wrong: a chunk ending with an alphanumeric character always
    getting a space, chunks being stripped before joining, newlines being used
    instead of spaces, joining being cut short when `insertNewLines` runs out,
    and the degenerate inputs of empty chunks, a single chunk and no chunk at
    all.

--- test/test_cpp.cpp
+++ test/test_cpp.cpp
@@ -466,6 +466,42 @@ TEST(KiwiCpp, Space)
 		u8"자세한 건 https://kiwipiepy.readthedocs.io를 보세요");
 }
 
+TEST(KiwiCpp, Glue)
+{
+	Kiwi& kiwi = reuseKiwiInstance();
+	using Chunks = std::vector<std::u16string>;
+	std::vector<bool> spaceInsertions;
+
+	EXPECT_EQ(kiwi.glue(Chunks{ u"그러나  알고보니 그 봉", u"지 안에 있던 것은 바로", u"레몬이었던 것이다." },
+		{}, &spaceInsertions), u"그러나  알고보니 그 봉지 안에 있던 것은 바로 레몬이었던 것이다.");
+	EXPECT_EQ(spaceInsertions, std::vector<bool>({ false, true }));
+
+	EXPECT_EQ(kiwi.glue(Chunks{ u"한국어", u"형태소분석기" }), u"한국어 형태소분석기");
+	// 왼쪽 조각이 영숫자로 끝나는 경우에는 항상 공백을 삽입한다.
+	EXPECT_EQ(kiwi.glue(Chunks{ u"abc", u"def" }), u"abc def");
+	EXPECT_EQ(kiwi.glue(Chunks{ u"2020", u"년에는" }), u"2020 년에는");
+	EXPECT_EQ(kiwi.glue(Chunks{ u"  앞뒤공백  ", u"  제거되나  " }), u"앞뒤공백 제거되나");
+	EXPECT_EQ(kiwi.glue(Chunks{ u"첫째 줄이다", u"둘째 줄이다" }, { true, true }), u"첫째 줄이다\n둘째 줄이다");
+
+	EXPECT_EQ(kiwi.glue(Chunks{ u"단일조각" }, {}, &spaceInsertions), u"단일조각");
+	EXPECT_TRUE(spaceInsertions.empty());
+	EXPECT_EQ(kiwi.glue(std::vector<std::u16string>{}, {}, &spaceInsertions), u"");
+	EXPECT_TRUE(spaceInsertions.empty());
+	EXPECT_EQ(kiwi.glue(Chunks{ u"", u"" }, {}, &spaceInsertions), u" ");
+	EXPECT_EQ(spaceInsertions, std::vector<bool>({ true }));
+
+	// insertNewLines가 조각 수보다 짧은 경우 값이 모자라는 지점에서 결합이 중단된다.
+	const Chunks six = { u"오늘은 날씨가", u"참 좋아서", u"산책을 나갔다",
+		u"가는 길에", u"친구를 만났고", u"함께 커피를 마셨다" };
+	EXPECT_EQ(kiwi.glue(six, { true }, &spaceInsertions), u"오늘은 날씨가\n참 좋아서");
+	EXPECT_EQ(spaceInsertions, std::vector<bool>({ true }));
+	EXPECT_EQ(kiwi.glue(six, { true, false }, &spaceInsertions),
+		u"오늘은 날씨가\n참 좋아서 산책을 나갔다");
+	EXPECT_EQ(spaceInsertions, std::vector<bool>({ true, true }));
+
+	EXPECT_EQ(kiwi.glue(std::vector<std::string>{ u8"한국어", u8"형태소분석기" }), u8"한국어 형태소분석기");
+}
+
 TEST(KiwiCpp, Pretokenized)
 {
 	Kiwi& kiwi = reuseKiwiInstance();
