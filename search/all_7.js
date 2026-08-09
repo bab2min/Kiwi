711ce30c2635bb19b0055124e5ebcd757f2c6353
bab2min <bab2min@users.noreply.github.com>
2026-08-09 15:30:58 +0000

    deploy: e010e9996395f22f3b32260f793c02bb05ba2a67

--- search/all_7.js
+++ search/all_7.js
@@ -17,7 +17,8 @@ var searchData=
   ['getform_14',['getForm',['../structkiwi_1_1Morpheme.html#a407488702f65e624bddc05a586742976',1,'kiwi::Morpheme']]],
   ['getmodeltype_15',['getModelType',['../classkiwi_1_1KiwiBuilder.html#aa58260b9f8c1087f854cb3bb953bd3ba',1,'kiwi::KiwiBuilder']]],
   ['gettypoform_16',['getTypoForm',['../classkiwi_1_1Kiwi.html#aec622c9828a27aad2c12a021e1c8a577',1,'kiwi::Kiwi']]],
-  ['groupid_17',['groupId',['../structkiwi_1_1MorphemeRaw.html#a79a39a11768abac9b1dcf72ebb06b5d2',1,'kiwi::MorphemeRaw']]],
-  ['gyeonggi_18',['gyeonggi',['../Types_8h.html#aa9fc09d985bc896c97e2e0f5cd41c96fa84c0e232e583410d3c16e081f1df9668',1,'kiwi']]],
-  ['gyeongsang_19',['gyeongsang',['../Types_8h.html#aa9fc09d985bc896c97e2e0f5cd41c96fa7ca8deade90717ea30fd8c03a6e072b2',1,'kiwi']]]
+  ['glue_17',['glue',['../classkiwi_1_1Kiwi.html#a462b62303e4b5fd834430772e37620c0',1,'kiwi::Kiwi::glue(const std::vector&lt; std::u16string &gt; &amp;textChunks, const std::vector&lt; uint8_t &gt; &amp;insertNewLines={}, std::vector&lt; uint8_t &gt; *spaceInsertionsOut=nullptr) const'],['../classkiwi_1_1Kiwi.html#a3b8e56b3207f6032b786e0fc4ff8a124',1,'kiwi::Kiwi::glue(const std::vector&lt; std::string &gt; &amp;textChunks, const std::vector&lt; uint8_t &gt; &amp;insertNewLines={}, std::vector&lt; uint8_t &gt; *spaceInsertionsOut=nullptr) const']]],
+  ['groupid_18',['groupId',['../structkiwi_1_1MorphemeRaw.html#a79a39a11768abac9b1dcf72ebb06b5d2',1,'kiwi::MorphemeRaw']]],
+  ['gyeonggi_19',['gyeonggi',['../Types_8h.html#aa9fc09d985bc896c97e2e0f5cd41c96fa84c0e232e583410d3c16e081f1df9668',1,'kiwi']]],
+  ['gyeongsang_20',['gyeongsang',['../Types_8h.html#aa9fc09d985bc896c97e2e0f5cd41c96fa7ca8deade90717ea30fd8c03a6e072b2',1,'kiwi']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -5,5 +5,6 @@ var searchData=
   ['getdefaulttyposet_2',['getDefaultTypoSet',['../TypoTransformer_8h.html#a9c6996f12769a9e6ae5cc8cb16c7e8bb',1,'kiwi']]],
   ['getform_3',['getForm',['../structkiwi_1_1Morpheme.html#a407488702f65e624bddc05a586742976',1,'kiwi::Morpheme']]],
   ['getmodeltype_4',['getModelType',['../classkiwi_1_1KiwiBuilder.html#aa58260b9f8c1087f854cb3bb953bd3ba',1,'kiwi::KiwiBuilder']]],
-  ['gettypoform_5',['getTypoForm',['../classkiwi_1_1Kiwi.html#aec622c9828a27aad2c12a021e1c8a577',1,'kiwi::Kiwi']]]
+  ['gettypoform_5',['getTypoForm',['../classkiwi_1_1Kiwi.html#aec622c9828a27aad2c12a021e1c8a577',1,'kiwi::Kiwi']]],
+  ['glue_6',['glue',['../classkiwi_1_1Kiwi.html#a462b62303e4b5fd834430772e37620c0',1,'kiwi::Kiwi::glue(const std::vector&lt; std::u16string &gt; &amp;textChunks, const std::vector&lt; uint8_t &gt; &amp;insertNewLines={}, std::vector&lt; uint8_t &gt; *spaceInsertionsOut=nullptr) const'],['../classkiwi_1_1Kiwi.html#a3b8e56b3207f6032b786e0fc4ff8a124',1,'kiwi::Kiwi::glue(const std::vector&lt; std::string &gt; &amp;textChunks, const std::vector&lt; uint8_t &gt; &amp;insertNewLines={}, std::vector&lt; uint8_t &gt; *spaceInsertionsOut=nullptr) const']]]
 ];
